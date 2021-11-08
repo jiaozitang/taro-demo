7f48a047097d2898b1e410eb49a2f9a485c683bb
tangjiao11 <tangjiao11@jd.com>
2021-11-08 17:17:28 +0800

    init

--- src/pages/index/component/MyCoverImage/index.tsx
+++ src/pages/index/component/MyCoverImage/index.tsx
@@ -15,7 +15,7 @@ export default class MyButton extends Component {
     return (
       <View className='container'>
         <View className='box'>box</View>
-        <CoverImage src='https://img20.360buyimg.com/ling/jfs/t1/76659/10/2396/85831/5d0b39baE5575c401/bb6f5767d549e728.png'
+        <CoverImage className='coverImage' src='https://img20.360buyimg.com/ling/jfs/t1/76659/10/2396/85831/5d0b39baE5575c401/bb6f5767d549e728.png'
           onLoad={() => {
             console.log('load')
           }}
