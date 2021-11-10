3c74df2d74eb225265306470bf55c9ce4d56d8d6
tangjiao11 <tangjiao11@jd.com>
2021-11-10 09:20:15 +0800

    feat: demo

--- src/pages/index/component/MyButton/index.tsx
+++ src/pages/index/component/MyButton/index.tsx
@@ -15,19 +15,19 @@ export default class MyButton extends Component {
     return (
       <View className='container'>
         {/* <Label>123</Label> */}
-        <Button type='default'  className='test' value='test'>type default</Button>
-        <Button type='primary' value='test'>type primary </Button>
-        <Button type='warn' value='test'>type warn </Button>
-        <Button type='default'  className='test' value='test' plain>type default</Button>
-        <Button type='primary' value='test' plain>type primary </Button>
-        <Button type='warn' value='test' plain>type warn </Button>
-        <Button type='default' value='test' disabled >type default  disabled</Button>
-        <Button type='primary' value='test' disabled >type primary  disabled</Button>
-        <Button type='warn' value='test' disabled >type warn  disabled</Button>
-        <Button type='default' value='test' loading >type default  disabled loading</Button>
-        <Button type='primary' value='test' loading >type primary  disabled loading</Button>
-        <Button type='warn' value='test' loading>type warn  disabled loading</Button>
-        <Button type='default' value='test' onClick={() => {
+        <Button type='default'  className='test' >type default1</Button>
+        <Button type='primary' >type primary </Button>
+        <Button type='warn' >type warn </Button>
+        <Button type='default'  className='test'  plain>type default</Button>
+        <Button type='primary'  plain>type primary </Button>
+        <Button type='warn'  plain>type warn </Button>
+        <Button type='default'  disabled >type default  disabled</Button>
+        <Button type='primary'  disabled >type primary  disabled</Button>
+        <Button type='warn'  disabled >type warn  disabled</Button>
+        <Button type='default'  loading >type default  disabled loading</Button>
+        <Button type='primary'  loading >type primary  disabled loading</Button>
+        <Button type='warn'  loading>type warn  disabled loading</Button>
+        <Button type='default'  onClick={() => {
           console.log('click')
         }} onTouchStart={() => {
           console.log('ontouchstart')
