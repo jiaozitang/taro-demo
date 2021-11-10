c880aafce1ee9cb3f0e3c6d941cfd0b2ddb163af
tangjiao11 <tangjiao11@jd.com>
2021-11-10 08:02:12 +0800

    feat: demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { View, Text, Image, CoverImage } from '@tarojs/components'
+import { View, Text, CoverView } from '@tarojs/components'
 import './index.scss'
 
 export default class Index extends Component {
@@ -20,14 +20,15 @@ export default class Index extends Component {
         <Text>Hello world!</Text>
         <View className='box'>123</View>
         {/* <Image src='https://img20.360buyimg.com/ling/jfs/t1/76659/10/2396/85831/5d0b39baE5575c401/bb6f5767d549e728.png' /> */}
-        <CoverImage className='coverImage' src='https://img20.360buyimg.com/ling/jfs/t1/76659/10/2396/85831/5d0b39baE5575c401/bb6f5767d549e728.png'
-          onLoad={() => {
-            console.log('load')
-          }}
-          onError={() => {
-            console.log('error')
-          }}
-        />
+        <CoverView className='cover-view'  onClick={() => {
+          console.log('click')
+        }} >
+          <CoverView className="flex-wrp">
+            <CoverView className="flex-item demo-text-1"></CoverView>
+            <CoverView className="flex-item demo-text-2"></CoverView>
+            <CoverView className="flex-item demo-text-3"></CoverView>
+          </CoverView>
+        </CoverView>
       </View>
     )
   }
