d4d6cc7b8a8fb013a77706b1f434bb0d92a75d89
tangjiao11 <tangjiao11@jd.com>
2021-11-10 21:10:49 +0800

    feat: demo

--- src/pages/index/component/MyButton/index.tsx
+++ src/pages/index/component/MyButton/index.tsx
@@ -29,25 +29,29 @@ export default class MyButton extends Component {
       <View className='container'>
         {
           arr.map(i => {
-            return <Button type={i} loading={loading} size={isMini ? 'mini' : 'default'} plain={plain} disabled={disabled}>{i}</Button>
+            return <Button className='btn' key={i} type={i} loading={loading} size={isMini ? 'mini' : 'default'} plain={plain} disabled={disabled}>{i}</Button>
           })
         }
-        <Button onClick={() => {
+        <Button className='btn' onClick={() => {
           this.setState({
             isMini: !this.state.isMini
           })
+        }} onTouchStart={() => {
+          console.log('taro touchstart')
+        }} onTouchEnd={() => {
+          console.log('taro touchend')
         }}>{this.state.isMini ? '取消' : '设置'}以上按钮Size mini属性</Button>
-        <Button onClick={() => {
+        <Button className='btn' onClick={() => {
           this.setState({
             plain: !this.state.plain
           })
         }}>{this.state.plain ? '取消' : '设置'}以上按钮plain属性</Button>
-        <Button onClick={() => {
+        <Button className='btn' onClick={() => {
           this.setState({
             loading: !this.state.loading
           })
         }}>{this.state.loading ? '取消' : '设置'}以上按钮loading属性</Button>
-        <Button onClick={() => {
+        <Button className='btn' onClick={() => {
           this.setState({
             disabled: !this.state.disabled
           })
