4ca053bb26855d38c70b93bad4649003bb9f6099
tangjiao11 <tangjiao11@jd.com>
2021-11-14 21:55:00 +0800

    feat: demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -18,10 +18,14 @@ export default class Index extends Component {
     return (
       <View className='index'>
         <Text>Hello world!</Text>
-        <View className='box'>123</View>
+        <View className='box' onTouchStart={() => {
+          console.log('onTouchStart')
+        }}>123</View>
         {/* <Image src='https://img20.360buyimg.com/ling/jfs/t1/76659/10/2396/85831/5d0b39baE5575c401/bb6f5767d549e728.png' /> */}
-        <CoverView className='cover-view'  onClick={() => {
-          console.log('click')
+        <CoverView className='cover-view' hoverClass='hoverclass'  onClick={() => {
+          console.log('click2')
+        }} onTouchStart={() => {
+          console.log('onTouchStart')
         }} >
           <CoverView className="flex-wrp">
             <CoverView className="flex-item demo-text-1"></CoverView>
