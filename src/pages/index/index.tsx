2b840ad1f7723b8e6a4af967d1ee132e2ca5a805
tangjiao11 <tangjiao11@jd.com>
2021-11-22 16:49:42 +0800

    feat: demo

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,38 +1,74 @@
 import { Component } from 'react'
-import { View, Text, CoverView } from '@tarojs/components'
+import { View, Text, Picker } from '@tarojs/components'
 import './index.scss'
 
-export default class Index extends Component {
+export default class PagePicker extends Component {
+  state = {
+    selector: [
+      {
+        name: '美国',
+        id: 'aK1'
+      }
+    ],
+    selectorChecked: 'a',
+    timeSel: '12:01',
+    dateSel: '2018-04-22'
+  }
 
-  componentWillMount () { }
+  onChange = e => {
+    console.log(e)
+    this.setState({
+      selectorChecked: this.state.selector[e.detail.name]
+    })
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onTimeChange = e => {
+    this.setState({
+      timeSel: e.detail.value
+    })
+  }
+  onDateChange = e => {
+    this.setState({
+      dateSel: e.detail.value
+    })
+  }
 
   render () {
+    console.log(12)
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <View className='box' onTouchStart={() => {
-          console.log('onTouchStart')
-        }}>123</View>
-        {/* <Image src='https://img20.360buyimg.com/ling/jfs/t1/76659/10/2396/85831/5d0b39baE5575c401/bb6f5767d549e728.png' /> */}
-        <CoverView className='cover-view' hoverClass='hoverclass'  onClick={() => {
-          console.log('click2')
-        }} onTouchStart={() => {
-          console.log('onTouchStart')
-        }} >
-          <CoverView className="flex-wrp">
-            <CoverView className="flex-item demo-text-1"></CoverView>
-            <CoverView className="flex-item demo-text-2"></CoverView>
-            <CoverView className="flex-item demo-text-3"></CoverView>
-          </CoverView>
-        </CoverView>
+      <View className='container'>
+        <View className='page-body'>
+          <View className='page-section'>
+            <Text>地区选择器</Text>
+            <View>
+              <Picker rangeKey='name' mode='selector' range={this.state.selector} onChange={this.onChange}>
+                <View className='picker'>
+                  当前选择：{this.state.selectorChecked}
+                </View>
+              </Picker>
+            </View>
+          </View>
+          <View className='page-section'>
+            <Text>时间选择器</Text>
+            <View>
+              <Picker mode='time' onChange={this.onTimeChange}>
+                <View className='picker'>
+                  当前选择：{this.state.timeSel}
+                </View>
+              </Picker>
+            </View>
+          </View>
+          <View className='page-section'>
+            <Text>日期选择器</Text>
+            <View>
+              <Picker mode='date' onChange={this.onDateChange}>
+                <View className='picker'>
+                  当前选择：{this.state.dateSel}
+                </View>
+              </Picker>
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
